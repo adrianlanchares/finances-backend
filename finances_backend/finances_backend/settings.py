33d6b3538175f3aa55c08f0e2f5aec34cf0e3a8e
adrianlanchares <adrianlopezlanchares@gmail.com>
2025-11-20 15:44:05 +0100

    allow docker container name as host

--- finances_backend/finances_backend/settings.py
+++ finances_backend/finances_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback_key")
 DEBUG = True
 
-ALLOWED_HOSTS = ["finances.backend.coco.homelab"]
+ALLOWED_HOSTS = ["finances.backend.coco.homelab", "finances-backend"]
 CORS_ALLOW_ALL_ORIGINS = True
 
 
